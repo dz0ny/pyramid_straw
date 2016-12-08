cfcb4f16af5b6c3c7618c3e63aecaf073b03f2d7
Maksym Shalenyi <supamaxy@gmail.com>
2016-12-08 14:33:54 +0200

    fixed no 'straw_data' issue

--- pyramid_straw/profiler/__init__.py
+++ pyramid_straw/profiler/__init__.py
@@ -65,7 +65,7 @@ def add_to_debug_toolbar(data):
     """Inject analyzed data in request."""
     request = get_current_request()
 
-    if not request and getattr(request, 'straw_data', None):
+    if not (request and getattr(request, 'straw_data', None)):
         return
 
     with lock:
